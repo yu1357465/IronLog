3f823bcfdb3653b104f439d16d6a27ddec5bcb50
yu1357465 <1035754866@qq.com>
2026-02-26 16:54:50 +0000

    feat: implement M4 history data retrieval in dashboard

--- core/urls.py
+++ core/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
+    #用户访问首页时，直接打入控制台视图
+    path('',views.dashboard_view,name='home'),
+
     #M1权限机制路由
     path('auth/login/',views.login_view,name='login'),
     path('auth/register/',views.register_view,name='register'),
@@ -9,4 +12,7 @@ urlpatterns=[
 
     #仪表盘入口(M1保护节点)
     path('dashboard/',views.dashboard_view,name='dashboard'),
+
+    #M3录入节点(Workout Logger)
+    path('train/log/',views.workout_logger_view,name='workout_logger'),
 ]
--- core/views.py
+++ core/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.decorators import login_required
+from .forms import WorkoutLogForm
+from .models import WorkoutLog
 
 def register_view(request):
     """处理用户注册，确认物理隔离边界"""
@@ -35,5 +37,42 @@ def logout_view(request):
 
 @login_required(login_url='login') #强制拦截器：未登录直接重定向
 def dashboard_view(request):
-    """私有核心数据展示节点(M1保护)"""
+    """私有核心数据展示节点(M1保护)&历史记录提取(M4)"""
+    # 1. 数据提取逻辑：去数据库里捞取数据
+    # 必须用 filter 加上强身份隔离，且按时间倒序排列（最新的在最前面）
+    user_logs=WorkoutLog.objects.filter(user=request.user).order_by('-date')
+
+    # 2. 封装数据包 (Context)
+    # 这里的 'logs' 是传递给前端的插槽。
+    context={
+        'logs':user_logs
+    }
+
+    # 3. 抛给前端渲染
     return render(request, 'core/dashboard.html')
+
+def workout_logger_view(request):
+    """处理M3的写入逻辑"""
+    if request.method=='POST':
+        #1.拦截数据：将前端POST过来的数据塞进过滤器
+        form=WorkoutLogForm(request.POST)
+
+        #2.规则校验：如果数据完全符合models.py中定义的类型和边界
+        if form.is_valid():
+            #3.拦截物理写入：先在内存中生成对象，但不保存到数据库(commit=False)
+            log_instance=form.save(commit=False)
+
+            #4.强制身份绑定：将当前发起请求的物理用户，烙印在数据上
+            log_instance.user=request.user
+
+            #5.物理写入：安全落库
+            log_instance.save()
+
+            #录入成功后，按照常规流转，跳回控制台
+            return redirect('dashboard')
+    else:
+        #如果是GET请求，说明用户刚打开页面，塞一个空的表单给他
+        form=WorkoutLogForm()
+
+    #将包含插槽变量的上下文(Context)传给前端模板
+    return render(request,'core/workout_logger.html',{'form':form})
